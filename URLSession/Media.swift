0d952f612de59215085fc8c5947dd1951a5aff6b
nahyar <mahyar.sh021@yahoo.com>
2023-01-18 09:49:21 +0330

    inited

---
+++ URLSession/Media.swift
@@ -0,0 +1,24 @@
+//
+//  Media.swift
+//  URLSession
+//
+//  Created by Mahyar on 1/16/23.
+//
+
+import UIKit
+
+struct Media {
+    let key: String
+    let filename: String
+    let data: Data
+    let mimeType: String
+    
+    init?(withImage image: UIImage, forKey key: String) {
+        self.key = key
+        self.mimeType = "image/jpeg"
+        self.filename = "kyleleeheadiconimage234567.jpg"
+        
+        guard let data = image.jpegData(compressionQuality: 0.7) else { return nil }
+        self.data = data
+    }
+}
